b39e9ad14082203723d0a68377b1209f6993d9df
SupriyaBY <supriyabyadav.june22@gmail.com>
2023-07-25 00:06:53 +0530

    Third Commit

---
+++ AllureClear.bat
@@ -0,0 +1,5 @@
+@ECHO
+ECHO Heyyy Supii....
+E:
+allure generate E:\SharadSirSeleniumBatch\BasicPrograms\Jan2023POMSeries\allure-results --clean -o E:\SharadSirSeleniumBatch\BasicPrograms\Jan2023POMSeries\allure-results
+PAUSE
---
+++ MyFirstBatch.bat
@@ -0,0 +1,5 @@
+@ECHO
+ECHO Heyyy Supii....
+E:
+cd E:\SharadSirSeleniumBatch\BasicPrograms\Jan2023POMSeries
+mvn clean test
